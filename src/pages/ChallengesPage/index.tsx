b6fddf19be3fd5a9b35bdd5d161160a51f342eb9
chruscinskiDiego <contatodiegochruscinski@gmail.com>
2025-10-20 15:54:45 -0300

    feat: ChallengesPage fetch

--- src/pages/ChallengesPage/index.tsx
+++ src/pages/ChallengesPage/index.tsx
@@ -1,5 +1,28 @@
+import { useEffect, useState } from "react";
+import { api } from "../../lib/axios";
+
 const ChallengesPage: React.FC = () => {
 
+    const [challenges, setChallenges] = useState();
+
+    const getChallenges = async () => {
+
+        const response = await api.get("/challenge/student-view/all");
+
+        if(response.status === 200){
+
+            setChallenges(response.data);
+
+        }
+    };
+
+    useEffect(() => {
+
+        getChallenges();
+
+    },[]);
+    
+    console.log('challenges: ' + JSON.stringify(challenges));
     return (
         <h1>Challenges Page</h1>
     );
