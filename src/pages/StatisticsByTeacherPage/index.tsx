18ea1be5355b3a72ff26fdbc7946bda8ce831d35
chruscinskiDiego <contatodiegochruscinski@gmail.com>
2025-10-23 02:51:42 -0300

    feat (statistics): Fetchs

--- src/pages/StatisticsByTeacherPage/index.tsx
+++ src/pages/StatisticsByTeacherPage/index.tsx
@@ -1,6 +1,30 @@
+import { useEffect, useState } from "react";
+import { api } from "../../lib/axios";
+
 const StatisticsByTeacherPage: React.FC = () => {
 
-    return(
+    const [statistics, setStatistics] = useState();
+
+    const getStatistics = async () => {
+
+        const response = await api.get("/course/statistics");
+
+        if (response.status === 200) {
+
+            setStatistics(response.data);
+
+        }
+    };
+
+    useEffect(() => {
+
+        getStatistics();
+
+    }, []);
+
+    console.log(JSON.stringify(statistics));
+
+    return (
         <h1>Statistics By Teacher</h1>
     );
 }
